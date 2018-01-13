b5ba2ea85195ceb7396026299f6ed5514c359edc
dokman <chaoticanthrim@hotmail.com>
2018-01-13 12:42:03 +0100

    preparing first commits to connect the client and server

--- KoiAnime Client/MainWindow.xaml.cs
+++ KoiAnime Client/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KoiAnime_Client.UserLibs;
 using MahApps.Metro.Controls;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace KoiAnime_Client
@@ -23,15 +25,59 @@ namespace KoiAnime_Client
     public partial class MainWindow : MetroWindow
     {
         RestClient client = new RestClient("http://localhost:5550/");
+        public static List<string> InvalidJsonElements;
+        //Title[] topRated;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        #region Methods
+
+        /// <summary>
+        /// Deserialize to Ilist<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonString"></param>
+        /// <returns></returns>
+        public static IList<T> DeserializeToList<T>(string jsonString)
+        {
+            InvalidJsonElements = null;
+            var array = JArray.Parse(jsonString);
+            IList<T> objectsList = new List<T>();
+
+            foreach (var item in array)
+            {
+                try
+                {
+                    // CorrectElements
+                    objectsList.Add(item.ToObject<T>());
+                }
+                catch (Exception ex)
+                {
+                    InvalidJsonElements = InvalidJsonElements ?? new List<string>();
+                    InvalidJsonElements.Add(item.ToString());
+                }
+            }
+
+            return objectsList;
+        }
+        #endregion
+
         private void Pages_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            TabItem tabItem = e.Source as TabItem;
 
+            switch(tabItem.Name)
+            {
+                case "TopRated":
+                    var request = new RestRequest("{id}", Method.GET);
+                    request.AddUrlSegment("id", "TopRated");
+                    IRestResponse response = client.Execute(request);
+                    var content = response.Content;
+                    break;
+            }
         }
     }
 }
